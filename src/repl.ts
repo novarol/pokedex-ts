ba4f0e06c6f171b7109ca4d082b6e05fea089398
novarol <novarol3d@gmail.com>
2026-02-21 22:12:35 +0100

    feat: implement repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,23 @@
+import { stdin, stdout } from "node:process";
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input.trim().toLowerCase().split(" ").filter((x) => x !== "");
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > ",
+  });
+
+  rl.prompt();
+  rl.on("line", (input) => {
+    const clean = cleanInput(input);
+    if (clean.length !== 0) {
+      console.log(`Your command was: ${clean[0]}`);
+    }
+    rl.prompt();
+  })
 }
