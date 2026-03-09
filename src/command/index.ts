2c0d368c6f20935e458b315026a9f255305c82de
novarol <novarol3d@gmail.com>
2026-03-09 18:00:15 +0100

    feat: add inspect command

--- src/command/index.ts
+++ src/command/index.ts
@@ -3,6 +3,7 @@ import { commandCatch } from "./catch.js";
 import { commandExit } from "./exit.js";
 import { commandExplore } from "./explore.js";
 import { commandHelp } from "./help.js";
+import { commandInspect } from "./inspect.js";
 import { commandMapBack, commandMapForward } from "./map.js";
 
 export function getCommands(): Record<string, CLICommand> {
@@ -36,6 +37,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "catch <pokemon-name>",
       description: "Attempt to catch Pokemon",
       callback: commandCatch,
+    },
+    inspect: {
+      name: "inspect <pokemon-name>",
+      description: "Look up Pokemon in the Pokedex",
+      callback: commandInspect,
     },
   };
 }
---
+++ src/command/inspect.ts
@@ -0,0 +1,30 @@
+import { type State } from "src/state.js";
+
+export async function commandInspect (state: State, ...args: string[]) {
+  const pokemonName = args.shift();
+
+  if (!pokemonName) {
+    throw new Error("you didn't provide a pokemon name");
+  }
+
+  const pokemon = state.pokedex[pokemonName]
+
+  if (!pokemon) {
+    throw new Error("you have not caught that pokemon yet");
+  }
+
+  console.log();
+  console.log("Name:", pokemon.name);
+  console.log("Height:", pokemon.height);
+  console.log("Weight:", pokemon.weight);
+  console.log("Stats:")
+  for (const stat of pokemon.stats) {
+    console.log(`  - ${stat.stat.name}:`, stat.base_stat);
+  }
+  console.log("Types:");
+  for (const type of pokemon.types) {
+    console.log(`  - ${type.type.name}`);
+  }
+  console.log();
+  
+}
